29ce97f8b5d04206ae7e8cebebe5590a4e12ec99
HashakGik <l.lorello@campus.uniurb.it>
2023-11-26 18:52:48 +0100

    .

--- bg.pl
+++ bg.pl
@@ -44,6 +44,22 @@ any_composition(random).
 line(X) :- non_diag(X), X \= grid.
 line(X) :- diag(X).
 
+house(C) :- extract_op_and_chld(C, stack, [C1, C2]), extract_shape(C1, triangle), extract_shape(C2, square), same_size(_, [C1, C2]).
+car(C) :- extract_op_and_chld(C, side_by_side, [C1, C2]), extract_shape(C1, circle), extract_shape(C2, circle), same_size(_, [C1, C2]), same_color(_, [C1, C2]).
+tower(C) :- extract_op_and_chld(C, stack, L), same_shape(square, L), same_size(_, L), length(L, N), N >= 2, N =< 3.
+wagon(C) :- extract_op_and_chld(C, side_by_side, L), same_shape(square, L), same_size(_, L), length(L, N), N >= 2, N =< 3.
+traffic_light(C) :- extract_op_and_chld(C, stack, [C1, C2, C3]), same_shape(circle, [C1, C2, C3]), same_size(_, [C1, C2, C3]), extract_color(C1, red), extract_color(C2, yellow), extract_color(C3, green).
+named_object(house).
+named_object(car).
+named_object(tower).
+named_object(wagon).
+named_object(traffic_light).
+is_named_object(C, house) :- house(C).
+is_named_object(C, car) :- car(C).
+is_named_object(C, tower) :- tower(C).
+is_named_object(C, wagon) :- wagon(C).
+is_named_object(C, traffic_light) :- traffic_light(C).
+
 shape_props(T, SH, CO, SZ) :- atom(T), term_string(T, S), split_string(S, "_", "", L), L = [SH, CO, SZ].
 extract_shape(T, SH) :- shape_props(T, SH1, _, _), term_string(SH, SH1), shape(SH).
 extract_color(T, CO) :- shape_props(T, _, CO1, _), term_string(CO, CO1), color(CO).
